1c70e12465a6d312ced24b14b64cbbd46d1241a9
bereket <bfrezghi81@gmail.com>
2020-01-12 15:40:26 -0600

    deployer modified

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,13 @@
 docker build -t bereketdeb1/multi-client:latest -t bereketdeb1/multi-client:$SHA -f ./client/Dockerfile ./client 
 docker build -t bereketdeb1/multi-server:latest -t bereketdeb1/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t bereketdeb1/multi-worker:latest -t bereketdeb1/multi-worker:$SHA -f ./worker/Dockerfile ./worker 
-docker push bereketdeb1/multi-client: latest
-docker push bereketdeb1/multi-server: latest
-docker push bereketdeb1/multi-worker: latest
-
-docker push bereketdeb1/multi-client: $SHA
-docker push bereketdeb1/multi-server: $SHA
-docker push bereketdeb1/multi-worker: $SHA
+docker push bereketdeb1/multi-client:latest
+docker push bereketdeb1/multi-server:latest
+docker push bereketdeb1/multi-worker:latest
+docker push bereketdeb1/multi-client:$SHA
+docker push bereketdeb1/multi-server:$SHA
+docker push bereketdeb1/multi-worker:$SHA
 kubectl apply -f k8
-kubectl set image deployments/server-deployment server=bereketdeb1/multi-server: $SHA
-kubectl set image deployments/client-deployment client=bereketdeb1/multi-client: $SHA
-kubectl set image deployments/worker-deployment worker=bereketdeb1/multi-worker: $SHA
+kubectl set image deployments/server-deployment server=bereketdeb1/multi-server:$SHA
+kubectl set image deployments/client-deployment client=bereketdeb1/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=bereketdeb1/multi-worker:$SHA
